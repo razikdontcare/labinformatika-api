bfca9b84345bf0d494c341a4b99e57ce2725797c
razikdontcare <gamersia4@gmail.com>
2025-01-17 22:28:34 +0800

    feat: enhance server logging and update response structure

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(async (c, next) => {
+  const { method, url } = c.req;
+  const day = new Date().toLocaleDateString();
+  const time = new Date().toLocaleTimeString();
+  console.log(`[${day} ${time}] ${method} ${url}`);
+  await next();
+});
 
-const port = 3000
-console.log(`Server is running on http://localhost:${port}`)
+app.get("/", (c) => {
+  return c.json({ msg: "Lab Informatika Official API", version: "1.0.0" });
+});
+
+const port = 8000;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
